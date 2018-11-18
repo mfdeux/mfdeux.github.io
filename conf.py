988922e27ec90d956a52536e59d5d85450b12251
Marc Ford <mrfxyz567@gmail.com>
2018-11-17 22:21:51 -0500

    Nikola auto commit.
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "mfdx"  # (translatable)
 BLOG_TITLE = "mfdx"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://mfdeux.github.io"
+SITE_URL = "https://mfdeux.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
